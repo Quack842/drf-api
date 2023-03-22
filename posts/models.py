9b367c0c0e381380dbbede90f8f08f9b8692550d
Nicolene Marais <nici.marais.ie@gmail.com>
2023-03-22 14:40:40 +0000

    follow along Challenge Extra: Adding the Image_Filters

--- posts/models.py
+++ posts/models.py
@@ -7,18 +7,36 @@ class Post(models.Model):
     Post Model, Related to 'owner', i.e a User instance.
     Default imahe set so thay we can always reference image.url.
     """
+    image_filter_choices = [
+        ('_1977', '1977'),
+        ('brannan', 'Brannan'),
+        ('earlybird', 'Earlybird'),
+        ('hudson', 'Hudson'),
+        ('inkwell', 'Inkwell'),
+        ('lofi', 'Lo-Fi'),
+        ('kelvin', 'Kelvin'),
+        ('normal', 'Normal'),
+        ('nashville', 'Nashville'),
+        ('rise', 'Rise'),
+        ('toaster', 'Toaster'),
+        ('valencia', 'Valencia'),
+        ('walden', 'Walden'),
+        ('xpro2', 'X-pro II')
+    ]
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../default_post_yyxugh',
-        blank=True
+        upload_to='images/', default='../default_post_yyxugh', blank=True
+    )
+    image_filter = models.CharField(
+        max_length=32, choices=image_filter_choices, default='normal'
     )
 
     class Meta:
-        ordering = ['created_at']
+        ordering = ['-created_at']
 
-        def __str__(self):
-            return f'{self.id} {self.title}'
+    def __str__(self):
+        return f'{self.id} {self.title}'
--- posts/serializers.py
+++ posts/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Profile
+from posts.models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -8,14 +8,29 @@ class PostSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
+    def validate_image(self, value):
+        if value.size > 1024 * 1024 * 2:
+            raise serializers.ValidationError(
+                'Image size larger than 2MB'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width is larger than 4096px'
+            )
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image width is larger than 4096px'
+            )
+        return value
+
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
 
     class Meta:
-        model = Profile
+        model = Post
         fields = [
-            'id', 'owner', 'created_at', 'updated_at', 'name',
+            'id', 'owner', 'created_at', 'updated_at',
             'content', 'image', 'is_owner', 'profile_image',
-            'profile_id'
+            'profile_id', 'image_filter'
         ]
