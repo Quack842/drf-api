e2ec92b36d66f4558c8acba96e117dfbe1c0a237
Nicolene Marais <nici.marais.ie@gmail.com>
2023-03-29 08:33:08 +0000

    still don't understand the filtering shit...

--- profiles/views.py
+++ profiles/views.py
@@ -22,7 +22,8 @@ class ProfileList(generics.ListAPIView):
         DjangoFilterBackend,
     ]
     filterset_fields = [
-        'owner__following__followed__profile'
+        'owner__following__followed__profile',
+        'owner__followed__owner__profile'
     ]
     ordering_fileds = [
         'posts_count',
